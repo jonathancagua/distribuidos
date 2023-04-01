63a73f5b4a9ca526d82728ae3c55e949c2272e6e
Jonathan Cagua <Jonathan.cagua@easymetering.com>
2023-04-01 12:22:08 -0500

    ADD: tp2

--- main/main.c
+++ main/main.c
@@ -4,12 +4,13 @@
 #include "config.h"
 
 const char *TAG = "MAIN";
-static bool bandera = true;
+static bool bandera = false;
 static const char* topico = "test/ledjc";
 static const char* topico_rss = "test/topicjc";
 void app_1(void);
 void app_2(void);
 void app_3(void);
+void app_4(void);
 /*******************************************************************************
  Programa principal
 ******************************************************************************/
@@ -36,7 +37,7 @@ void funcion(const char * topic, const char * data){
 void app_main(void)
 {
 
-	app_3();
+	app_4();
 
 }
 void app_0(){
@@ -61,7 +62,6 @@ void app_0(){
 
 	}
 }
-
 
 void app_1(void){
 	WIFI_init();
@@ -129,6 +129,92 @@ void app_3(void)
 	while(1){
 		CRONO_delayMs(1000);
 	}
+	SD_unmount();
+
+}
+
+/**
+Requerimientos de la actividad:
+El nodo debe poder configurar la red WiFi, la dirección de broker y la
+cantidad de mediciones a realizar (no mayor a 100)
+leyendo estos parámetros desde un archivo de texto config.txt en la SD.
+
+El nodo debe sensar la señal RSSI y guardar en un archivo CSV lecturas.csv una tabla con
+las lecturas tomadas periodicamente. Las lecturas deben hacerse con un delay de 1 segundo
+y la tabla debe tener dos columnas: una para el tiempo <timestamp-GMT>
+ y la otra para la potencia <RSSI>.
+
+Además, cada lectura de potencia realizada debe transmitirse en tiempo real por MQTT al
+ cliente en la PC, donde se deberá mostrar por consola cada lectura recibida con su timestamp asociado.
+
+Desde la App deben poder iniciarse las capturas. Además debe configurarse un LED
+en el mismo dashboard para que se prenda cuando el nodo haya recibido el comando
+de inicio y se apague cuando se haya concluído y guardado todos los datos en el nodo.
+ */
+void app_4(void)
+{
+
+	SD_mount();
+	FILE *f = SD_open("config.txt", "r");
+	int rssi;
+	char timestamp[64]="";
+	char msg[100];
+
+	int64_t epoch;
+	char user_cantidad[32];
+	char user_broken[40]={0};
+	char user_ssid[40]={0};
+	char user_pw[40]={0};
+	SD_gets(f, user_ssid, sizeof(user_ssid));
+	ESP_LOGI(TAG, "SSID %s",user_ssid);
+	SD_gets(f, user_pw, sizeof(user_pw));
+	ESP_LOGI(TAG, "PWD %s",user_pw);
+	SD_gets(f, user_broken, sizeof(user_broken));
+	ESP_LOGI(TAG, "user_broken %s",user_broken);
+	SD_gets(f, user_cantidad, sizeof(user_cantidad));
+	ESP_LOGI(TAG, "user_cantidad %s",user_cantidad);
+	int n = strlen(user_ssid);
+	user_ssid[n-1]='\0';
+	n = strlen(user_pw);
+	user_pw[n-1]='\0';
+	n = strlen(user_broken);
+	user_broken[n-1]='\0';
+	int muestreo = atoi(user_cantidad);
+	WIFI_userInit(user_ssid,user_pw);
+	MQTT_userInit(user_broken);
+	MQTT_subscribe_handler(&funcion);
+	MQTT_subscribe("test/ledjc");
+	MQTT_publish(topico, "apagado");
+	CRONO_sntpInit();
+	while(1){
+		if(true == bandera){
+			if(muestreo <= 100){
+				f = SD_open("lecturas.csv", "w");
+				for (int var = 0; var < muestreo; ++var) {
+					CRONO_delayMs(1000);
+					rssi = WIFI_getRSSI();
+					epoch = CRONO_getTime(timestamp, sizeof(timestamp));
+					SD_printf(f, "%i %s \n",rssi, timestamp);
+					sprintf(msg,"%s %i",timestamp,rssi);
+					MQTT_publish(topico_rss, msg);
+					ESP_LOGI(TAG, "Enviando");
+					if(false == bandera){
+						break;
+						ESP_LOGI(TAG, "Stop grabanding");
+					}
+				}
+				ESP_LOGI(TAG, "Se Cierra SD");
+				bandera = false;
+				MQTT_publish(topico, "apagado");
+				SD_close(f);
+			}
+			else{
+				ESP_LOGE(TAG, "Asignando muestreo");
+			}
+		}
+		CRONO_delayMs(1000);
+	}
+
 	SD_unmount();
 
 }
